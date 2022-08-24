f416fb1d25435aefe43ff4b5e6056b2b6076841f
NiksKozlov <nikskozlov@gmail.com>
2022-08-24 18:18:44 +0300

    Add CSS and structure code

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import styles from './Message.module.css'
 
-function Message() {
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
     return (
-        <div>
-
+        <div className={styles.container}>
+            <img className={styles.ava}
+                 src={props.avatar}
+                 alt="img" />
+            <div className={styles.messageBox}>
+                <p className={styles.name}>{props.name}</p>
+                <p className={styles.message}>{props.message}</p>
+                <p className={styles.time}>{props.time}</p>
+            </div>
+            <div className={styles.triangle}></div>
         </div>
     )
 }
