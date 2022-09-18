005b654a82dfdcd87e5b03f9f0c3263358378f5c
NiksKozlov <nikskozlov@gmail.com>
2022-09-18 16:04:30 +0300

    Add Routes and new components Jun, Jun+

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom';
 
 function Header() {
     return (
         <div>
-            // add NavLinks
-
+            <NavLink to={'/'}>PreJunior </NavLink>
+            <NavLink to={'/junior'}>Junior </NavLink>
+            <NavLink to={'/junior+'}>Junior+</NavLink>
         </div>
     )
 }
