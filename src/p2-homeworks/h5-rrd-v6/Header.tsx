191fbf5c9437e25b5901a6e26058c595f41b0ab1
NiksKozlov <nikskozlov@gmail.com>
2022-09-25 02:20:52 +0300

    Code refactoring and add css in HW5. Activate and doing HW6

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -6,8 +6,8 @@ import {PATH} from './Pages';
 function Header() {
     return (
         <div className={s.sideBar}>
-                <NavLink to={PATH.PRE_JUNIOR} className={s.navLink}>pre-junior </NavLink>
-                <NavLink to={PATH.JUNIOR} className={s.navLink}>junior </NavLink>
+                <NavLink to={PATH.PRE_JUNIOR} className={s.navLink} >pre-junior</NavLink>
+                <NavLink to={PATH.JUNIOR} className={s.navLink}>junior</NavLink>
                 <NavLink to={PATH.JUNIOR_PLUS} className={s.navLink}>junior+ </NavLink>
             <div className={s.btn}></div>
         </div>
